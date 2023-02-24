4594f007e00b4c75f34d52c1decff20169f198e6
santhoshsaravanamuthu <santhoshs.csbs2020@citchennai.net>
2023-02-24 13:59:39 +0530

    first code

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import cors from 'cors';
-import bodyParser from 'body-parser';
 import Connection from './database/db.js';
 import Router from './routes/route.js';
 
@@ -10,8 +9,8 @@ dotenv.config();
 const app = express();
 
 app.use(cors());
-app.use(bodyParser.json({ extended: true }));
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.json({ extended: true }));
+app.use(express.urlencoded({ extended: true }));
 app.use('/', Router);
 
 const PORT = process.env.PORT || 8000;
